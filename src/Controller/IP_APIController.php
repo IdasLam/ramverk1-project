b9c1042e5047a9f06ff2f8b3d184d212e5d5dd14
Ida Lam <idaslam@outlook.com>
2020-11-06 12:43:15 +0100

    created a controller for IP validator API

---
+++ src/Controller/IP_APIController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Anax\Controller;
+
+use Anax\Commons\ContainerInjectableInterface;
+use Anax\Commons\ContainerInjectableTrait;
+
+// use Anax\Route\Exception\ForbiddenException;
+// use Anax\Route\Exception\NotFoundException;
+// use Anax\Route\Exception\InternalErrorException;
+
+/**
+ * A sample controller to show how a controller class can be implemented.
+ * The controller will be injected with $di if implementing the interface
+ * ContainerInjectableInterface, like this sample class does.
+ * The controller is mounted on a particular route and can then handle all
+ * requests for that mount point.
+ *
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ */
+class IP_APIController implements ContainerInjectableInterface
+{
+    use ContainerInjectableTrait;
+
+    public function indexActionPost()
+    {
+        $input =  json_decode($this->di->request->getBody());
+        $type = "none";
+        $valid = false;
+        $domain = NULL;
+        $ip = NULL;
+
+
+        if (gettype($input) === "object" && $input->ip) {
+            $ip = $input->ip;
+
+            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+                $type = "IPv4";
+                $valid = true;
+            } elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+                $valid = true;
+                $type = "IPv6";
+            }
+    
+            if (filter_var($ip, FILTER_VALIDATE_IP)) {
+                $domain = gethostbyaddr($ip);
+            }
+        }
+
+        return [[
+            "ip" => $ip,
+            "type" => $type,
+            "valid" => $valid,
+            "domain" => $domain,
+        ]];
+    }
+}
