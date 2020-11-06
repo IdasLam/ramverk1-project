265f48e238880ac1d26e147d221c52d974b65ba4
Ida Lam <idaslam@outlook.com>
2020-11-06 12:44:46 +0100

    ip API mounted on /ip-validator

---
+++ config/router/1100_ipAPI.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Flat file controller for reading markdown files from content/.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Flat file content controller.",
+            "mount" => "ip-validator",
+            "handler" => "\Anax\Controller\IP_APIController",
+        ],
+    ]
+];
