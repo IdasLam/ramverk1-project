41a17d3e6804e525d6560d8875a7ded3f4c7c78f
Ida Lam <idaslam@outlook.com>
2020-11-10 23:59:39 +0100

    description about the API, print out the data that comes from the API

--- view/ip-validator.php
+++ view/ip-validator.php
@@ -4,10 +4,14 @@
     <p>It will in turn return something like this:</p>
     <code>
     {
-        "ip": "216.58.211.142",
-        "type": "IPv4",
-        "valid": "true",
-        "domain": "arn09s10-in-f14.1e100.net"
+        "ip":"216.58.211.142",
+        "type":"IPv4",
+        "valid":true,
+        "domain":"arn09s10-in-f142.1e100.net",
+        "city":"Mountain View",
+        "region":"California",
+        "country":"US",
+        "loc":"37.4056,-122.0775"
     }
     </code>
     <h2>Test ip</h2>
@@ -17,7 +21,7 @@
 </div>
 <form method="get">
     <h1>Test your ip here</h1>
-    <input type="text" name="ip-input" placeholder="ip adress">
+    <input type="text" name="ip-input" placeholder="ip adress" value=<?= $_SERVER['REMOTE_ADDR'] ?>>
     <button>Check</button>
 
 <?php
@@ -25,7 +29,7 @@ if ($validatedIp) {
     ?>
     <h2>Results</h2>
     <div id="information">
-        <table>
+        <table style={}>
             <tr>
                 <th>IP</th>
                 <th>Type</th>
@@ -36,13 +40,25 @@ if ($validatedIp) {
                 <td><?= $validatedIp->ip ?></td>
                 <td><?= $validatedIp->type ?></td>
                 <td><?= $validatedIp->valid ? "true" : "false" ?></td>
-                <td><?= $validatedIp->domain ?></td>
+                <td><?= $validatedIp->domain ?? "null" ?></td>
+            </tr>
+            <tr>
+                <th>City</th>
+                <th>Region</th>
+                <th>Country</th>
+                <th>Loc</th>
+            </tr>
+            <tr>
+                <td><?= $validatedIp->city ?? "null" ?></td>
+                <td><?= $validatedIp->region ?? "null" ?></td>
+                <td><?= $validatedIp->country ?? "null" ?></td>
+                <td><?= $validatedIp->loc ?? "null" ?></td>
             </tr>
         </table>
         <h3>JSON format</h3>
         <a href="ip?ip-json=<?= $validatedIp->ip ?>"><?= $validatedIp->ip ?></a>
         <p id="data">
-        <code>
+        <code style="max-width:50%;overflow-wrap:anywhere;">
             <?php echo json_encode($validatedIp) ?>
         </code>
         </p>
