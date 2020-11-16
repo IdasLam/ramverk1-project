d384e4790f4c165d7146358b02f5ec0014d7cbce
Ida Lam <idaslam@outlook.com>
2020-11-16 20:31:09 +0100

    created an init and get raw methods in weather class

--- src/Weather/Weather.php
+++ src/Weather/Weather.php
@@ -3,10 +3,32 @@ namespace Anax\Weather;
 
 class Weather
 {
+    private $res;
 
-    public function test()
+    public function init(string $city, string $country)
     {
-        echo "test";
+        if ($_SERVER['SERVER_NAME'] == "localhost") {
+            $url = "http://web/htdocs/ip-validator";
+        } else {
+            $url = $_SERVER['SERVER_NAME'] . "htdocs/weatherAPI";
+        }
+
+        $data = array('city' => $city, "country" => $country);
+
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/json\r\n",
+                'method'  => 'POST',
+                'content' => json_encode($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $this->res = file_get_contents($url, false, $context);
+    }
+
+    public function getRaw()
+    {
+        return $this->res;
     }
 }
 
