bfc0a45122dbf6a203c8081493e32e6c25338319
Ida Lam <idaslam@outlook.com>
2020-11-16 20:30:27 +0100

    added fetcher

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -5,6 +5,21 @@ namespace Anax\Controller;
 use Anax\Commons\ContainerInjectableInterface;
 use Anax\Commons\ContainerInjectableTrait;
 
+function fetcher($ip, $url)
+{
+    $data = array('ip' => $ip);
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'POST',
+            'content' => json_encode($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    return file_get_contents($url, false, $context);
+}
+
 /**
  * A sample controller to show how a controller class can be implemented.
  * The controller will be injected with $di if implementing the interface
@@ -28,7 +43,7 @@ class WeatherController implements ContainerInjectableInterface
     {
         $input =  $this->di->request->getGet('ip-input');
         $json =  $this->di->request->getGet('ip-json');
-        $viewData;
+        $weather = $this->di->get("weather");
         $data = ["validatedIp" => null];
         
         if ($this->di->request->getBaseUrl() == null) {
@@ -38,21 +53,25 @@ class WeatherController implements ContainerInjectableInterface
         }
 
         if ($input) {
-            $viewData = fetcher($input, $url);
-            $data = ["validatedIp" => json_decode($viewData)];
+            $res = json_decode(fetcher($input, $url));
+            $city = $res->city;
+            $country = $res->country;
+
+            $weather->init($city, $country);
         }
         
-
+        var_dump($weather->getRaw());
+        // echo $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    
         $this->di->get('page')->add('weather', $data);
 
         if ($json) {
             return fetcher($json, $url);
         }
 
-        $weather = $this->di->get("weather");
         // echo $thi->di->has("Weather");
 
-        $weather->test();
+        // $weather->test();
         
         return $this->di->get('page')->render([
             "title" => "IP Validator"
