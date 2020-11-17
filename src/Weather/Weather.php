d694fde802758c8e60e43f94e30e308069b213de
Ida Lam <idaslam@outlook.com>
2020-11-17 16:51:04 +0100

    weather class parse location and fetches from weatherAPI

--- src/Weather/Weather.php
+++ src/Weather/Weather.php
@@ -3,17 +3,21 @@ namespace Anax\Weather;
 
 class Weather
 {
-    private $res;
+    private $res = null;
 
-    public function init(string $city, string $country)
+    public function init(string $loc)
     {
         if ($_SERVER['SERVER_NAME'] == "localhost") {
-            $url = "http://web/htdocs/ip-validator";
+            $url = "http://web/htdocs/weatherAPI";
         } else {
             $url = $_SERVER['SERVER_NAME'] . "htdocs/weatherAPI";
         }
 
-        $data = array('city' => $city, "country" => $country);
+        $loc = explode(",", $loc);
+        $lat = $loc[0];
+        $lon = $loc[1];
+
+        $data = array("lat" => $lat, "lon" => $lon);
 
         $options = array(
             'http' => array(
@@ -23,12 +27,45 @@ class Weather
             )
         );
         $context  = stream_context_create($options);
-        $this->res = file_get_contents($url, false, $context);
+        $this->res = json_decode(file_get_contents($url, false, $context))->result;
     }
 
     public function getRaw()
     {
-        return $this->res;
+        return json_encode($this->res);
+    }
+
+    public function getWeek()
+    {
+        if ($this->res != null) {
+            $current = $this->res->current;
+            $week = $this->res->daily;
+
+            // var_dump($this->res);
+            // var_dump($current, $week);
+
+            $currentWeek = [];
+            $currentWeek[] = [
+                "day" => date('D', $current->dt),
+                "date" => date('d/m', $current->dt),
+                "weather" => $current->weather[0]->description,
+            ];
+
+
+            foreach ($week as $day) {
+                if ($day == $week[0]) {
+                    continue;
+                }
+
+                $currentWeek[] = [
+                    "day" => date('D', $day->dt),
+                    "date" => date('d/m', $day->dt),
+                    "weather" => $day->weather[0]->description,
+                ];
+            }
+
+            return $currentWeek;
+        }
     }
 }
 
