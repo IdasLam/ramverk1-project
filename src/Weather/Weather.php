ec4af90585dca53a392794b34b665b5469a25f76
Ida Lam <idaslam@outlook.com>
2020-11-21 17:23:27 +0100

    added functionality to get parsed history

--- src/Weather/Weather.php
+++ src/Weather/Weather.php
@@ -3,7 +3,9 @@ namespace Anax\Weather;
 
 class Weather
 {
-    private $res = null;
+    private $raw = null;
+    private $forcast = null;
+    private $history = null;
 
     public function init(string $lon, string $lat)
     {
@@ -23,44 +25,75 @@ class Weather
             )
         );
         $context  = stream_context_create($options);
-        $this->res = json_decode(file_get_contents($url, false, $context))->result;
+        $this->raw = file_get_contents($url, false, $context);
+
+        $this->forcast = json_decode($this->raw)->forcast;
+        $this->history = json_decode($this->raw)->history;
     }
 
     public function getRaw()
     {
-        return json_encode($this->res);
+        return $this->raw;
     }
 
     public function getWeek()
     {
-        if ($this->res != null) {
-            $current = $this->res->current;
-            $week = $this->res->daily;
-
-            // var_dump($this->res);
-            // var_dump($current, $week);
-
-            $currentWeek = [];
-            $currentWeek[] = [
-                "day" => date('D', $current->dt),
-                "date" => date('d/m', $current->dt),
-                "weather" => $current->weather[0]->description,
-            ];
-
+        if ($this->forcast != null) {
+            $week = $this->forcast->daily;
 
             foreach ($week as $day) {
-                if ($day == $week[0]) {
-                    continue;
-                }
-
                 $currentWeek[] = [
                     "day" => date('D', $day->dt),
                     "date" => date('d/m', $day->dt),
+                    "minTemp" => $day->temp->min,
+                    "maxTemp" => $day->temp->max,
                     "weather" => $day->weather[0]->description,
                 ];
             }
 
             return $currentWeek;
+        }
+    }
+
+    public function getHistory()
+    {
+        if ($this->history != null) {
+            $history = $this->history;
+
+            // var_dump($history);
+            foreach($history as $day) {
+                $maxTemp = 0;
+                $minTemp = 0;
+                $description = null;
+                $dayDate = null;
+                $date = null;
+                
+                foreach($day as $hour) {
+                    $dayDate = date('D', $hour->dt);
+                    $date = date('d/m', $hour->dt);
+
+                    if ($maxTemp < $hour->temp) {
+                        $maxTemp = $hour->temp;
+                    }
+    
+                    if ($minTemp == null) {
+                        $minTemp = $hour->temp;
+                    } elseif ($minTemp > $hour->temp) {
+                        $minTemp = $hour->temp;
+                    }
+                    $description = $hour->weather[0]->description;
+                }
+
+                $historydays[] = [
+                    "day" => $dayDate,
+                    "date" => $date,
+                    "maxTemp" => $maxTemp,
+                    "minTemp" => $minTemp,
+                    "description" => $description,
+                ];
+            }
+
+            return $historydays;
         }
     }
 }
