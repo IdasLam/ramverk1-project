f31fc1a6cbb1a72bbf8fe994607b27911e8f5a3a
Ida Lam <idaslam@outlook.com>
2020-11-21 19:23:13 +0100

    added mcrul, pararell requests

--- src/Controller/WeatherAPIController.php
+++ src/Controller/WeatherAPIController.php
@@ -28,38 +28,76 @@ class WeatherAPIController implements ContainerInjectableInterface
         $res = "Could not get weather report from lontitude and latitude.";
 
         if (gettype($input) === "object" && $input->lat && $input->lon) {
-
             $APIKey = $_ENV["OPENWEATHERAPP"];
             $lat = trim($input->lat);
             $lon = trim($input->lon);
-            
-            $url = "https://api.openweathermap.org/data/2.5/onecall?lat={$lat}&lon={$lon}&exclude=minutely,hourly&units=metric&appid={$APIKey}";
-            
-            $handler = curl_init();
-            
-            curl_setopt($handler, CURLOPT_URL, $url);
-            curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
-            
-            $forcast = json_decode(curl_exec($handler));
-            curl_close($handler);
-            
+
+            $urls = ["https://api.openweathermap.org/data/2.5/onecall?lat={$lat}&lon={$lon}&exclude=minutely,hourly&units=metric&appid={$APIKey}"];
+
             for ($day = 1; $day < 6; $day++) {
                 $date = date(strtotime("today - {$day} days"));
                 
-                $url = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={$lat}&lon={$lon}&dt={$date}&units=metric&&appid={$APIKey}";
-    
-                $handler = curl_init();
-    
-                curl_setopt($handler, CURLOPT_URL, $url);
-                curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
-    
-                $history[] = json_decode(curl_exec($handler))->hourly;
-                curl_close($handler);
+                $urls[] = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={$lat}&lon={$lon}&dt={$date}&units=metric&&appid={$APIKey}";
             }
+
+            $multi = curl_multi_init();
+            $channels = array();
+            
+            // Loop through the URLs, create curl-handles
+            // and attach the handles to our multi-request
+            foreach ($urls as $url) {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            
+                curl_multi_add_handle($multi, $ch);
+            
+                $channels[$url] = $ch;
+            }
+            
+            // While we're still active, execute curl
+            $active = null;
+            do {
+                $mrc = curl_multi_exec($multi, $active);
+            } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            
+            while ($active && $mrc == CURLM_OK) {
+                // Wait for activity on any curl-connection
+                if (curl_multi_select($multi) == -1) {
+                    continue;
+                }
+            
+                // Continue to exec until curl is ready to
+                // give us more data
+                do {
+                    $mrc = curl_multi_exec($multi, $active);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+            
+            // Loop through the channels and retrieve the received
+            // content, then remove the handle from the multi-handle
+            $res = [];
+
+            foreach ($channels as $channel) {
+                array_push($res, json_decode(curl_multi_getcontent($channel)));
+                curl_multi_remove_handle($multi, $channel);
+            }
+            
+            // Close the multi-handle and return our results
+            curl_multi_close($multi);
+        }
+
+        foreach ($res as $result) {
+            if ($result == $res[0]) {
+                continue;
+            }
+
+            $history[] = $result->hourly;
         }
 
         return  [[
-            "forcast" => $forcast ?? null,
+            "forcast" => $res[0] ?? null,
             "history" => $history ?? null
         ]];
     }
