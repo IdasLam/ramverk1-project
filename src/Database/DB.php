5760fa2ad1cfb31a1c2089157f643baa6166e230
Ida Lam <idaslam@outlook.com>
2020-12-15 19:03:15 +0100

    database file

---
+++ src/Database/DB.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ida\Database;
+
+class DB
+{
+    private $db;
+
+    public function __construct()
+    {
+        $options = [
+            "dsn"             => "sqlite:" . ANAX_INSTALL_PATH . "/data/db.sqlite",
+            "username"        => "root",
+            "password"        => "root",
+            "driver_options"  => [
+                \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+            ],
+            "fetch_mode"      => \PDO::FETCH_OBJ,
+            "table_prefix"    => null,
+            "session_key"     => "Anax\Database",
+            "emulate_prepares" => false,
+        
+            // True to be very verbose during development
+            "verbose"         => false,
+        
+            // True to be verbose on connection failed
+            "debug_connect"   => true,
+        ];
+
+        $this->db = new \Anax\Database\Database();
+        $this->db->setOptions($options);
+
+        $this->db->connect();
+    }
+}
