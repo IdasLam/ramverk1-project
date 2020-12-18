2e0995feb1c49b5c750e51fbd62264adf1a3ca3e
Ida Lam <idaslam@outlook.com>
2020-12-18 13:38:24 +0100

    created new methods for checking and creating a new user

--- src/Database/Users.php
+++ src/Database/Users.php
@@ -15,4 +15,38 @@ class Users extends DB
 
         return $this->db->executeFetchAll($sql);
     }
+
+    public function checkUser($username)
+    {
+        $sql = "SELECT * FROM users WHERE username LIKE ?";
+        $res = $this->db->executeFetchAll($sql, [$username]);
+
+        $inDatabase = sizeof($res) > 0;
+
+        return $inDatabase;
+    }
+
+    public function userExsists($username)
+    {
+        $sql = "SELECT * FROM users WHERE username LIKE ?";
+        $res = $this->db->executeFetchAll($sql, [$username]);
+
+        return count($res) > 0;
+    }
+
+    public function createUser($email, $username, $password)
+    {
+        $nameExsists = $this->userExsists($username);
+
+        if ($nameExsists == false) {
+            $hased_password = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (username, email, password) VALUES
+            (?, ?, ?)";
+            $this->db->execute($sql, [$username, $email, $password]);
+
+            return $this->checkUser($username);
+        }
+
+        return "something went wrong";
+    }
 }
