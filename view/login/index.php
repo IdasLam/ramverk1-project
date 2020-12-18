0fa3f662663dab359c167bd27fb71a5cf0675250
Ida Lam <idaslam@outlook.com>
2020-12-18 13:39:21 +0100

    finnished the basic login form

--- view/login/index.php
+++ view/login/index.php
@@ -2,4 +2,8 @@
 <form action="login/user" method="post">
     <label for="username">Username</label>
     <input name="username "type="text">
+    <label for="password">Password</label>
+    <input name="password "type="password">
+    <button>Login</button>
 </form>
+<button onclick="location.href = 'login/register';">Register</button>
