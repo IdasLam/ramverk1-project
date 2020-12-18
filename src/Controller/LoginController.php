9a46cff2d22e3585411c42b9749ae7598070fb34
Ida Lam <idaslam@outlook.com>
2020-12-18 13:38:36 +0100

    WIP, login and register page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -15,17 +15,48 @@ class LoginController implements ContainerInjectableInterface
     {
         $db = new \Ida\Database\DB();
 
-        $this->loggedIn = $this->di->session->get("loggedIn");
+        // $this->loggedIn = $this->di->session->get("loggedIn");
 
         // var_dump($this->loggedIn);
 
-        $data = [];
+        $data = ["title" => "Login"];
         $this->di->get('page')->add('login/index', $data);
+        return $this->di->get('page')->render($data);
     }
     
-    public function UserActionPost()
+    public function registerActionGet()
     {
-        var_dump("login");
+        // $this->loggedIn = $this->di->session->get("loggedIn");
+
+        // var_dump($this->loggedIn);
+
+        $data = ["title" => "Register", "createError" => $this->di->session->get("createError")];
+        $this->di->get('page')->add('login/register', $data);
+        return $this->di->get('page')->render($data);
+    }
+    
+    // public function UserActionPost()
+    // {
+    //     var_dump("login");
+
+    //     // $this->di->get('page')->add('home/index', $data);
+    // }
+
+    public function signupActionPost()
+    {
+        $user = new \Ida\Database\User();
+
+        $username = htmlentities($this->di->request->getPost("username"));
+        $password = htmlentities($this->di->request->getPost("password"));
+        $email = htmlentities($this->di->request->getPost("email"));
+        
+        $res = $user->createUser($email, $username, $password);
+
+        if ($res) {
+            $this->di->session->set("username", $username);
+        } elseif ($res === "something went wrong") {
+            $this->di->session->set("createError", $res);
+        }
 
         // $this->di->get('page')->add('home/index', $data);
     }
