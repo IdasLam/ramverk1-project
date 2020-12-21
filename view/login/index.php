c08652c446a5d02a53e7a6071e04321045761d3f
Ida Lam <idaslam@outlook.com>
2020-12-21 16:03:24 +0100

    input name were wrong

--- view/login/index.php
+++ view/login/index.php
@@ -1,9 +1,20 @@
+<?php
+    if ($di->session->get("userId") !== null) {
+        $di->response->redirect("home");
+    }
+?>
+
 <h1>login</h1>
+<?php if ($di->session->get("loginError") !== null) : ?>
+    <div class="error">
+        <p><?= $di->session->get("loginError") ?></p>
+    </div>
+<?php endif; ?>
 <form action="login/userlogin" method="post">
     <label for="username">Username</label>
-    <input name="username "type="text">
+    <input name="username" type="text" required>
     <label for="password">Password</label>
-    <input name="password "type="password">
+    <input name="password" type="password" required>
     <button>Login</button>
 </form>
 <button onclick="location.href = 'login/register';">Register</button>
--- view/login/register.php
+++ view/login/register.php
@@ -2,18 +2,18 @@
 <?php
     if ($di->session->get("loggedin")) :
         if (isset($createError)) : ?>
-            <div class="createError">
+            <div class="error">
                 <p>Oops! <?= $createError ?></p>
             </div>
     
 <?php endif; ?>
         <form action="signup" method="post">
             <label for="username">Username</label>
-            <input name="username "type="text">
+            <input name="username" type="text" required>
             <label for="email">email</label>
-            <input name="email "type="email">
+            <input name="email" type="email" required>
             <label for="password">Password</label>
-            <input name="password "type="password">
+            <input name="password" type="password" required>
             <button>Register</button>
         </form>
 <?php endif; ?>
