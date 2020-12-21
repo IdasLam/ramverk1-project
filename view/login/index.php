d16f488f7e9aa3a09f2ffc5dff5f37eaf9ee40cd
Ida Lam <idaslam@outlook.com>
2020-12-21 15:43:32 +0100

    updated action of post

--- view/login/index.php
+++ view/login/index.php
@@ -1,5 +1,5 @@
 <h1>login</h1>
-<form action="login/user" method="post">
+<form action="login/userlogin" method="post">
     <label for="username">Username</label>
     <input name="username "type="text">
     <label for="password">Password</label>
