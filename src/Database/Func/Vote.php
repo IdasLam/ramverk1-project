9659f310c4404a444a3c2cf1bba98752c328b29e
Ida Lam <idaslam@outlook.com>
2020-12-22 23:43:48 +0100

    changes so upvoting system works

--- src/Database/Func/Vote.php
+++ src/Database/Func/Vote.php
@@ -12,54 +12,68 @@ class Vote extends DB
 
     public function vote($username, $postid, $commentid, $vote)
     {
-        $sql = "INSERT INTO vote (username, postid, commentid, vote) VALUES (?, ?, ?, ?)";
-        $res = $this->db->execute($sql, [$username, $postid, $commentid, $vote]);
+        $commentid = $commentid === null ? null : intval($commentid);
+
+        $sql = "INSERT INTO votes (username, postid, commentid, vote) VALUES (?, ?, ?, ?)";
+        $res = $this->db->execute($sql, [$username, intval($postid), $commentid, $vote > 0 ? 1 : -1]);
     }
     
-    public function hasvotedPost($username, $postid, $commentid)
+    public function hasvotedPost($username, $postid)
     {
-        $sql = "SELECT * FROM vote WHERE postid LIKE ? AND $username LIKE ? AND commentid LIKE NULL";
-        $res = $this->db->executeFetch($sql, [$username, $postid, $commentid]);
+        $sql = "SELECT * FROM votes WHERE postid = ? AND username = ? AND commentid IS NULL";
+        $res = $this->db->executeFetch($sql, [$postid, $username]);
 
-        return count($res) > 0;
+        return $res != null ? $res->vote : null;
     }
 
-    public function votePost($id, $vote, $username) {
-        if ($vote == "upvote") {
-            $this->vote($username, $id, null, 1);
-            $sql = "UPDATE posts SET upvote = upvote + 1 WHERE id = ?";
-            $res = $this->db->execute($sql, [$id]);
+    public function votePost($postid, $vote, $username) {
+        // if ($vote === 1) {
+        //     $this->vote($username, $id, null, 1);
+        //     $sql = "UPDATE posts SET upvote = upvote + 1 WHERE id = ?";
+        //     $res = $this->db->execute($sql, [$id]);
             
-            $sql = "SELECT upvote FROM posts WHERE id = ?";
-        } else {
-            $this->vote($username, $id, null, 0);
-            $sql = "UPDATE posts SET downvote = downvote + 1 WHERE id = ?";
-            $res = $this->db->execute($sql, [$id]);
-            $sql = "SELECT downvote FROM posts WHERE id = ?";
-        }
+        //     $sql = "SELECT upvote FROM posts WHERE id = ?";
+        // } else {
+        //     $this->vote($username, $id, null, 0);
+        //     $sql = "UPDATE posts SET downvote = downvote + 1 WHERE id = ?";
+        //     $res = $this->db->execute($sql, [$id]);
+        //     $sql = "SELECT downvote FROM posts WHERE id = ?";
+        // }
 
-        $res = $this->db->executeFetch($sql, [$id]);
+        // $res = $this->db->executeFetch($sql, [$id]);
 
-        return json_encode($res);
+        // return json_encode($res);
+
+        $this->vote($username, $postid, null, $vote);
     }
 
     public function removeVotePost($postid, $vote, $username) {
-        $sql = "DELETE FROM posts WHERE id = ? AND username = ? AND commentid = null";
+        $sql = "DELETE FROM votes WHERE id = ? AND username = ? AND commentid IS NULL";
         $res = $this->db->execute($sql, [$postid, $username]);
-        
-        $sql = "DELETE FROM vote WHERE id = ? AND username = ? AND commentid = null";
-        $res = $this->db->execute($sql, [$postid, $username]);
-        
-        if ($vote == "upvote") {
+    }
+
+    public function voteStatusPost($postid, $vote)
+    {
+        if ($vote === 1) {
             $sql = "SELECT upvote FROM posts WHERE id = ?";
         } else {
             $sql = "SELECT downvote FROM posts WHERE id = ?";
         }
 
-        $res = $this->db->executeFetch($sql, [$id]);
-
-        var_dump($res);
+        $res = $this->db->executeFetch($sql, [$postid]);
 
         return json_encode($res);
+    }
+    
+    public function updateVotePost($postid, $vote, $username)
+    {
+        $voteScore = $vote > 0 ? 1 : -1;
+        ([$postid, $vote, $username, $voteScore]);
+        $sql = "UPDATE votes SET vote = $voteScore WHERE postid = ? AND username = ? AND commentid IS NULL";
+        // $sql = "UPDATE vote SET vote = $voteScore WHERE id = ? AND username = ? AND commentid = 0";
+
+        $this->db->execute($sql, [$postid, $username]);
+
+        // $this->votePost($postid, $vote, $username);
     }
 }
