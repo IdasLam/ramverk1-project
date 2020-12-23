a33a698d0b0714578ea6d36ec034e22b6305e3d6
Ida Lam <idaslam@outlook.com>
2020-12-23 17:23:27 +0100

    render post and added method that creates new posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,19 +9,47 @@ class PostController implements ContainerInjectableInterface
 {
     use ContainerInjectableTrait;
 
-    public function indexActionPost()
+    public function indexActionGet()
     {
-        $post = new \Ida\Database\Post();
-        $input =  json_decode($this->di->request->getBody());
+        $postdb = new \Ida\Database\Posts();
+        $vote = new \Ida\Database\Func\Vote();
+        $commentsdb = new \Ida\Database\Func\Comments();
 
-        // return $vote->votePost($postid, $type, $username);
+        $username = $this->di->session->get("username");
+
+        $id =  $this->di->request->getGet("id");
+        
+        $post = $postdb->fetchPost($id);
+        $comments = $commentsdb->postComments($id);
+
+        $data = [
+            "post" => $post,
+            "username" => $username,
+            "vote" => $vote,
+            "comments" => $comments,
+            "commentsdb" => $commentsdb
+        ];
+
+        if (isset($post)) {
+            $this->di->get('page')->add('post/post', $data);
+        } else {
+            $this->di->get('page')->add('post/index', $data);   
+        }
+
+        return $this->di->get('page')->render($data);
     }
     
-    public function postActionPost()
+    public function newpostActionPost()
     {
-        $post = new \Ida\Database\Post();
-        $input =  json_decode($this->di->request->getBody());
+        $post = new \Ida\Database\Posts();
+        $input = $this->di->request->getPost();
 
-        // return $vote->votePost($postid, $type, $username);
+        $tags = $this->di->request->getPost("tags");
+        $content = $this->di->request->getPost("content");
+        $username = $this->di->session->get("username");
+
+        $post->newPosts($tags, $content, $username);
+
+        return $this->di->response->redirect("home");
     }
 }
