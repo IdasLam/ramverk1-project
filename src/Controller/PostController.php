e1ff659a02de30d39a0ae55e844e01f26ea7a945
Ida Lam <idaslam@outlook.com>
2020-12-23 18:32:56 +0100

    added htmlenteties but also tag result

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -18,24 +18,38 @@ class PostController implements ContainerInjectableInterface
         $username = $this->di->session->get("username");
         $email = $this->di->session->get("email");
 
-        $id =  $this->di->request->getGet("id");
+        $tags = htmlentities(trim($this->di->request->getGet("tag")));
+        $id = htmlentities($this->di->request->getGet("id"));
         
-        $post = isset($id) ? $postdb->fetchPost($id) : $postdb->allLatestPosts();
-        $comments = $commentsdb->postComments($id);
+        if (isset($tags)) {
+            $post = $postdb->searchTags($tags);
 
-        $data = [
-            "posts" => $post,
-            "username" => $username,
-            "vote" => $vote,
-            "comments" => isset($id) ? $comments : null,
-            "commentsdb" => $commentsdb,
-            "gravatar" => isset($username) ? "https://www.gravatar.com/avatar/" . md5($email) : null
-        ];
+            $data = [
+                "posts" => $post,
+                "username" => $username,
+                "vote" => $vote,
+                "gravatar" => isset($username) ? "https://www.gravatar.com/avatar/" . md5($email) : null
+            ];
 
-        if (isset($id)) {
-            $this->di->get('page')->add('post/post', $data);
+            $this->di->get('page')->add('post/index', $data);
         } else {
-            $this->di->get('page')->add('post/index', $data);   
+            $post = isset($id) ? $postdb->fetchPost($id) : $postdb->allLatestPosts();
+            $comments = $commentsdb->postComments($id);
+    
+            $data = [
+                "posts" => $post,
+                "username" => $username,
+                "vote" => $vote,
+                "comments" => isset($id) ? $comments : null,
+                "commentsdb" => $commentsdb,
+                "gravatar" => isset($username) ? "https://www.gravatar.com/avatar/" . md5($email) : null
+            ];
+    
+            if (isset($id)) {
+                $this->di->get('page')->add('post/post', $data);
+            } else {
+                $this->di->get('page')->add('post/index', $data);   
+            }
         }
 
         return $this->di->get('page')->render($data);
@@ -44,14 +58,21 @@ class PostController implements ContainerInjectableInterface
     public function newpostActionPost()
     {
         $post = new \Ida\Database\Posts();
-        $input = $this->di->request->getPost();
 
-        $tags = $this->di->request->getPost("tags");
-        $content = $this->di->request->getPost("content");
+        $tags = htmlentities($this->di->request->getPost("tags"));
+        $content = htmlentities($this->di->request->getPost("content"));
         $username = $this->di->session->get("username");
 
         $post->newPosts($tags, $content, $username);
 
         return $this->di->response->redirect("home");
+    }
+    
+    public function searchTagActionGet()
+    {
+        $post = new \Ida\Database\Posts();
+        $search = htmlentities(trim($this->di->request->getGet("search")));
+
+        return $this->di->response->redirect("post?tag=" . $search);
     }
 }
