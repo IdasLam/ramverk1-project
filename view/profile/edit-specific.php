c11bc5e23b54d89cbea3de95eca64108f793cabe
Ida Lam <idaslam@outlook.com>
2021-01-07 18:46:07 +0100

    style changes

--- view/profile/edit-specific.php
+++ view/profile/edit-specific.php
@@ -1,25 +1,25 @@
 <div class="profile">
-    <?php if (isset($error)) : ?>
-        <div class="edit-error">
-            <p>Opps!</p>
-            <p><?= $error ?></p>
-        </div>
-    <?php endif; ?>
-
     <img src=<?= $gravatar ?> alt="profile-img">
-    <h1><?= $username ?></h1>
-    <p><?= $email ?></p>
-    <div class="edit">
-        <p>Edit <?= $edit ?></p>
-        <form action="edit" method="post">
-            <?php if ($edit === "username") : ?>
-            <input type="text" name=<?= $edit ?> placeholder=<?= $username ?>>
-            <?php elseif ($edit === "email") : ?>
-            <input type="text" name=<?= $edit ?> placeholder=<?= $email ?>>
-            <?php else : ?>
-            <input type="text" name=<?= $edit ?>>
-            <?php endif; ?>
-            <button name="edit" value=<?= $edit ?>>Edit</button>
-        </form>
+    <div class="edit grid gap-4">
+        <h1><?= $username ?></h1>
+        <p><?= $email ?></p>
+        <div class="edit">
+        <?php if (isset($error)) : ?>
+            <div class="edit-error font-bold">
+                <p>Ops! <?= $error ?></p>
+            </div>
+        <?php endif; ?>
+            <p>New <?= $edit ?>:</p>
+            <form action="edit" method="post">
+                <?php if ($edit === "username") : ?>
+                <input type="text" name="new" placeholder=<?= $username ?>>
+                <?php elseif ($edit === "email") : ?>
+                <input type="text" name="new" placeholder=<?= $email ?>>
+                <?php else : ?>
+                <input type="text" name="new">
+                <?php endif; ?>
+                <button name="edit" value=<?= $edit ?>>Edit</button>
+            </form>
+        </div>
     </div>
 </div>
--- view/profile/edit.php
+++ view/profile/edit.php
@@ -3,16 +3,18 @@
 ?>
 <div class="profile">
     <img src=<?= $gravatar ?> alt="profile-img">
-    <div class="edit-name">
-        <h1><?= $username ?></h1>
-        <a href="edit?type=username">🖊️</a>
-    </div>
-    <div class="edit-email">
-        <p><?= $email ?></p>
-        <a href="edit?type=email">🖊️</a>
-    </div>
-    <div class="edit-password">
-        <p>Password</p>
-        <a href="edit?type=password">🖊️</a>
+    <div class="grid gap-4">
+        <div class="edit-name">
+            <h1><?= $username ?></h1>
+            <a href="edit?type=username">🖊️</a>
+        </div>
+        <div class="edit-email">
+            <p><?= $email ?></p>
+            <a href="edit?type=email">🖊️</a>
+        </div>
+        <div class="edit-password">
+            <p>Password</p>
+            <a href="edit?type=password">🖊️</a>
+        </div>
     </div>
 </div>
